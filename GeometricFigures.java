69ed765f074f4cb1f5054a4268947606960251df
Salajan Paul-Sebastian <salajanpaul1@gmail.com>
2024-06-13 14:51:35 +0300

    .

--- GeometricFigures.java
+++ GeometricFigures.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 public class GeometricFigures extends JFrame {
     private JToggleButton circleButton;
     private JToggleButton rectangleButton;
+    private JToggleButton freeformButton;
     private JButton colorButton;
     private Color currentColor = Color.BLACK;
     private CustomShapePanel shapePanel;
@@ -21,14 +22,17 @@ public class GeometricFigures extends JFrame {
         JPanel controlPanel = new JPanel();
         circleButton = new JToggleButton("Circle");
         rectangleButton = new JToggleButton("Rectangle");
+        freeformButton = new JToggleButton("Mouse drag");
         colorButton = new JButton("Color");
 
         ButtonGroup group = new ButtonGroup();
         group.add(circleButton);
         group.add(rectangleButton);
+        group.add(freeformButton);
 
         controlPanel.add(circleButton);
         controlPanel.add(rectangleButton);
+        controlPanel.add(freeformButton);
         controlPanel.add(colorButton);
 
         colorButton.addActionListener(e -> {
@@ -53,6 +57,7 @@ public class GeometricFigures extends JFrame {
     private class CustomShapePanel extends JPanel implements MouseListener, MouseMotionListener {
         private ArrayList<CustomShape> shapes = new ArrayList<>();
         private CustomShape currentShape = null;
+        private FreeformCurve currentCurve = null;
 
         public CustomShapePanel() {
             addMouseListener(this);
@@ -71,6 +76,10 @@ public class GeometricFigures extends JFrame {
                 g2d.setColor(currentShape.color);
                 currentShape.draw(g2d);
             }
+            if (currentCurve != null) {
+                g2d.setColor(currentCurve.color);
+                currentCurve.draw(g2d);
+            }
         }
 
         @Override
@@ -79,6 +88,9 @@ public class GeometricFigures extends JFrame {
                 currentShape = new CustomCircle(e.getX(), e.getY(), currentColor);
             } else if (rectangleButton.isSelected()) {
                 currentShape = new CustomRectangle(e.getX(), e.getY(), currentColor);
+            } else if (freeformButton.isSelected()) {
+                currentCurve = new FreeformCurve(currentColor);
+                currentCurve.addPoint(e.getX(), e.getY());
             }
         }
 
@@ -89,6 +101,11 @@ public class GeometricFigures extends JFrame {
                 shapes.add(currentShape);
                 currentShape = null;
                 repaint();
+            } else if (currentCurve != null) {
+                currentCurve.addPoint(e.getX(), e.getY());
+                shapes.add(currentCurve);
+                currentCurve = null;
+                repaint();
             }
         }
 
@@ -96,6 +113,9 @@ public class GeometricFigures extends JFrame {
         public void mouseDragged(MouseEvent e) {
             if (currentShape != null) {
                 currentShape.setEnd(e.getX(), e.getY());
+                repaint();
+            } else if (currentCurve != null) {
+                currentCurve.addPoint(e.getX(), e.getY());
                 repaint();
             }
         }
@@ -109,5 +129,83 @@ public class GeometricFigures extends JFrame {
         @Override
         public void mouseMoved(MouseEvent e) {}
     }
-}
 
+    private abstract class CustomShape {
+        protected Color color;
+
+        public CustomShape(Color color) {
+            this.color = color;
+        }
+
+        public abstract void draw(Graphics2D g2d);
+        public abstract void setEnd(int x, int y);
+    }
+
+    private class CustomCircle extends CustomShape {
+        private int x, y, width, height;
+
+        public CustomCircle(int x, int y, Color color) {
+            super(color);
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public void draw(Graphics2D g2d) {
+            g2d.drawOval(x, y, width, height);
+        }
+
+        @Override
+        public void setEnd(int x, int y) {
+            width = Math.abs(x - this.x);
+            height = Math.abs(y - this.y);
+        }
+    }
+
+    private class CustomRectangle extends CustomShape {
+        private int x, y, width, height;
+
+        public CustomRectangle(int x, int y, Color color) {
+            super(color);
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public void draw(Graphics2D g2d) {
+            g2d.drawRect(x, y, width, height);
+        }
+
+        @Override
+        public void setEnd(int x, int y) {
+            width = Math.abs(x - this.x);
+            height = Math.abs(y - this.y);
+        }
+    }
+
+    private class FreeformCurve extends CustomShape {
+        private ArrayList<Point> points = new ArrayList<>();
+
+        public FreeformCurve(Color color) {
+            super(color);
+        }
+
+        public void addPoint(int x, int y) {
+            points.add(new Point(x, y));
+        }
+
+        @Override
+        public void draw(Graphics2D g2d) {
+            for (int i = 0; i < points.size() - 1; i++) {
+                Point p1 = points.get(i);
+                Point p2 = points.get(i + 1);
+                g2d.drawLine(p1.x, p1.y, p2.x, p2.y);
+            }
+        }
+
+        @Override
+        public void setEnd(int x, int y) {
+            addPoint(x, y);
+        }
+    }
+}
